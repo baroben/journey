47710c3380521612e1e3b74f57cdfe3501a2d4d3
baroben <86844407+baroben@users.noreply.github.com>
2022-06-07 09:08:25 +0200

    Deleted useless classes
    I have deleted old and useless classes that are not used in the project anymore.

--- tabbarportefolio.cpp
+++
@@ -1,9 +0,0 @@
-#include "tabbarportefolio.h"
-
-TabBarPortefolio::TabBarPortefolio()
-{
-    if(!portefolios.empty()){
-        for(PortefolioTab *p : portefolios)
-            this->addTab(p,"oui");
-    }
-}
